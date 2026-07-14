573d62e2d662397a431dda16e6377895110a3bbc
Nilotpal04 <nilotpalb2004@gmail.com>
2026-07-14 17:10:25 +0000

    create desktop application shell

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 function App() {
   return (
-    <main className="">
-
+    <main className="min-h-screen bg-[#FAF6F0] text-[#1C1A17]">
+      <div className="flex h-screen">
+        <aside className="w-20 border-r border-[#C9BBA8]">
+        </aside>
+        <div className="flex flex-1 flex-col">
+          <header className="h-20 border-b border-[#C9BBA8]">
+          </header>
+          
+          <section className="flex-1">
+          </section>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
+
+export default App;
