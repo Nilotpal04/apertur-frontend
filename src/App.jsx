a23bdc04ab62b49f946a5227feaaa71b0e275eb0
Nilotpal04 <nilotpalb2004@gmail.com>
2026-07-14 17:04:59 +0000

    chnage folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 function App() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold">
-        Apertur
-      </h1>
-    </main>
-  );
-}
+    <main className="">
 
-export default App;
+    </main>
+  )
+}
