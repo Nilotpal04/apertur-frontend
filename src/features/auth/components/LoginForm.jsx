f48e06fc7023e52aeaedc0efe23b5509dbbdb4b5
Nilotpal04 <nilotpalb2004@gmail.com>
2026-07-19 17:46:41 +0000

    refactor login form using React Hook  Form

--- src/features/auth/components/LoginForm.jsx
+++ src/features/auth/components/LoginForm.jsx
@@ -1,42 +1,50 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form"
+import { data } from "react-router-dom";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const {
+        register,
+        handleSubmit
+    } = useForm();
 
-  return (
-    <div className="w-full max-w-md rounded-3xl border border-[var(--contact-sheet)] bg-white/60 p-8">
+    const onSubmit = (data) => {
+        console.log(data);
+    };
 
-      <h1 className="mb-8 text-3xl font-semibold">
-        Welcome Back
-      </h1>
+    return (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="w-full max-w-md rounded-3xl border border-[var(--contact-sheet)] bg-white/60 p-8"
+        >
+            <h1 className="mb-8 text-3xl font-semibold">
+                Welcome back
+            </h1>
 
-      <div className="space-y-5">
+            <div className="space-y-5">
 
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="w-full rounded-xl border border-[var(--contact-sheet)] p-3 outline-none"
-        />
+                <input 
+                    type="email"
+                    placeholder="Email"
+                    {...register("email")}
+                    className="w-full rounded-xl border border-[var(--contact-sheet)] p-3 outline-none"
+                />
 
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="w-full rounded-xl border border-[var(--contact-sheet)] p-3 outline-none"
-        />
+                <input 
+                    type="passsword"
+                    placeholder="Password"
+                    {...register("password")}
+                    className="w-full rounded-xl border border-[var(--contact-sheet)] p-3 outline-none"
+                />
 
-        <button className="w-full rounded-xl bg-[var(--darkroom-ink)] py-3 text-[var(--bone-paper)]">
-          Sign In
-        </button>
-
-      </div>
-
-    </div>
-  );
+                <button
+                    type="submit"
+                    className="w-full rounded-xl bg-[var(--darkroom-ink)] py-3 text-[var(--bone-paper)]"
+                >
+                    Sign In
+                </button>
+            </div>
+        </form>
+    )
 }
 
 export default LoginForm;
