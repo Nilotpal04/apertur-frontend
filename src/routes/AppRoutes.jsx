84a7e6bb9823a459510b3ddeae850cca16783ba7
Nilotpal04 <nilotpalb2004@gmail.com>
2026-07-19 15:46:10 +0000

    create authetication feature and routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,15 +1,17 @@
 import { Routes, Route } from "react-router-dom";
+
 import Home from "../pages/Home";
+import Login from "../features/auth/pages/Login";
+import Register from "../features/auth/pages/Register";
 
 function AppRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
 
-      <Route
-        path="/"
-        element={<Home />}
-      />
+      <Route path="/login" element={<Login />} />
 
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
