89eda1a1a9830a7f50e1d7677a4ad18b04d0656e
Nilotpal04 <nilotpalb2004@gmail.com>
2026-07-20 14:54:25 +0000

    persist authentication state across sessions

--- src/store/auth.store.js
+++ src/store/auth.store.js
@@ -1,20 +1,30 @@
+import { Flashlight, LogIn } from "lucide-react";
+import { set } from "react-hook-form";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useAuthStore = create((set) => ({
-    accessToken: null,
-    isAuthenticated: false,
-
-    login: (token) => 
-        set({
-            accessToken: token,
-            isAuthenticated: true,
-        }),
-    
-    logout: () =>
-        set({
+const useAuthStore = create(
+    persist(
+        (set) => ({
             accessToken: null,
             isAuthenticated: false,
+
+            login: (token) =>
+                set({
+                    accessToken: token,
+                    isAuthenticated: true,
+                }),
+
+            logout: () => 
+                set({
+                    accessToken: null,
+                    isAuthenticated: false,
+                }),
         }),
-}));
+        {
+            name: "apertur-auth"
+        }
+    )
+)
 
 export default useAuthStore;
