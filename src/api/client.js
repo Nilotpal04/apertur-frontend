9cd193e1ca0c2898d05f8f810f64d8af5ef1150e
Nilotpal04 <nilotpalb2004@gmail.com>
2026-07-22 14:16:50 +0000

    configure Axios authentication interceptor

--- src/api/client.js
+++ src/api/client.js
@@ -1,11 +1,25 @@
 import axios from "axios";
+import useAuthStore from "../store/auth.store";
+import { configs } from "eslint-plugin-react-hooks";
 
 const client = axios.create({
-  baseURL: "http://localhost:8000",
-  timeout: 10000,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "http://localhost:8000"
 });
+
+client.interceptors.request.use(
+  (config) => {
+    const token = useAuthStore.getState().accessToken;
+    if(token){
+      config.headers.Authorization =
+          `Bearer ${token}`;
+    }
+
+    return config;
+  },
+
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default client;
